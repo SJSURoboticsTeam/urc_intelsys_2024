f7c989972cf2e4d1724ddf1f5b22310e415bc328
japjikbatra <japjikbatra@gmail.com>
2025-01-25 21:07:27 +0000

    subscription
    subscription

--- src/pathfinder/pathfinder/pathfinder_listener.py
+++ src/pathfinder/pathfinder/pathfinder_listener.py
@@ -1,1 +1,21 @@
-#pathfinder file
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+from constants import MAP_TOPIC, GOAL_TOPIC, QOS
+
+
+class PathfinderListener(Node):
+    def __init__(self):
+        super().__init__("pathfinder_listener")
+        self.create_subscription(
+            Float64, MAP_TOPIC, lambda heading: print(heading), QOS,
+            Float64, GOAL_TOPIC, lambda heading: print(heading), QOS
+        )
+
+
+def main():
+    rclpy.init()
+    try:
+        rclpy.spin(PathfinderListener())
+    except KeyboardInterrupt:
+        print("Shutting down")
