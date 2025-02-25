dbfacc2a636b1802f148669567bf88afbc34f10f
ALEX HOVAKIMYAN <hovak101@gmail.com>
2025-02-24 20:06:30 -0800

    fixed gps to cartesian coordinates inaccuracy

--- src/gps/gps/geo_to_cart_srv.py
+++ src/gps/gps/geo_to_cart_srv.py
@@ -23,15 +23,9 @@ class GeoToCartHandler(Node):
                 ("ref_longitude", 90),
             ],
         )
-        # semi-major axis constant
-        self.A = 6378137
-        # eccentricity constant
-        self.EE = 6.69437999013 * (10 ** (-3))
 
-        lat_rad = self.deg_to_rad(self.get_parameter("ref_latitude").value)
-        long_rad = self.deg_to_rad(self.get_parameter("ref_longitude").value)
-
-        self.x_ref, self.y_ref, self.z_ref = self.gnss_to_ecef(lat_rad, long_rad)
+        self.ref_lat_rad = self.deg_to_rad(self.get_parameter("ref_latitude").value)
+        self.ref_long_rad = self.deg_to_rad(self.get_parameter("ref_longitude").value)
 
         self.srv = self.create_service(
             GeoToCart, "geo_to_cart", self.geo_to_cart_callback
@@ -40,31 +34,6 @@ class GeoToCartHandler(Node):
 
     def deg_to_rad(self, deg):
         return deg * math.pi / 180
-
-    def gnss_to_ecef(self, lat, long):
-        # convert to gnss to ecef(cartesian coordinate with center of earth as origin)
-        denom = math.sqrt(1 - self.EE * math.sin(lat) ** 2)
-        x = (self.A / denom) * math.cos(lat) * math.cos(long)
-        y = (self.A / denom) * math.cos(lat) * math.sin(long)
-        z = (self.A * (1 - self.EE) / denom) * math.sin(lat)
-
-        return (x, y, z)
-
-    def ecef_to_enu(self, lat, long, x, y, z) -> tuple[float, float]:
-        # find distances
-        # convert ecef to enu (apply rotation matrix)
-        dx = x - self.x_ref
-        dy = y - self.y_ref
-        dz = z - self.z_ref
-
-        e = -1 * math.sin(long) * dx + math.cos(long) * dy
-        n = (
-            -1 * math.sin(lat) * math.cos(lat) * dx
-            - math.sin(lat) * math.sin(long) * dy
-            + math.cos(lat) * dz
-        )
-
-        return (e, n)
 
     def gnss_to_cart(self, lat: float, long: float) -> tuple[float, float]:
         """
@@ -81,9 +50,12 @@ class GeoToCartHandler(Node):
         lat_rad = self.deg_to_rad(lat)
         long_rad = self.deg_to_rad(long)
 
-        x, y, z = self.gnss_to_ecef(lat_rad, long_rad)
-        e, n = self.ecef_to_enu(lat_rad, long_rad, x, y, z)
-        return int(e), int(n)
+        R = 6378137
+        x = 2 * R * math.asin(math.cos(self.ref_lat_rad) 
+            * math.sin((long_rad - self.ref_long_rad) / 2))
+        y = R * (lat_rad - self.ref_lat_rad)
+        
+        return x, y
 
     def geo_to_cart_callback(self, request, response):
         """
