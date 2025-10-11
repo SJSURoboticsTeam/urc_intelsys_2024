7ff18c94ec4a3ba48827596a1b7607e7789195ad
japjikbatra <japjikbatra@gmail.com>
2025-10-11 00:55:05 +0000

    Basic Sift detection logic

--- src/autonomous_typing/autonomous_typing/keyboard_alignment.py
+++ src/autonomous_typing/autonomous_typing/keyboard_alignment.py
@@ -13,36 +13,55 @@ from ament_index_python.packages import get_package_share_directory
 class KeyboardAlignment(Node):
     def __init__(self):
         super().__init__("keyboard_alignment")
-        
+
         #subscribes to IMAGE_TOPIC
         self.create_subscription(Image, IMAGE_TOPIC, self.image_callback, QOS)
         #publishes PoseStamped message??
-        self.publisher_ = self.create_publisher(PoseStamped, "keyboard_alignment_topic", QOS)
+        #self.publisher_ = self.create_publisher(PoseStamped, "keyboard_alignment_topic", QOS)
 
-        #allows for you to have access to templatte image
+        #allows for you to have access to template image
         package_share_dir = get_package_share_directory("autonomous_typing")
-        template_image_path = os.path.join(package_share_dir, "resources", "template_image.jpg")
+        TEMPLATE_IMAGE_PATH = os.path.join(package_share_dir, "resources", "template_image.jpg")
         
         #load template image
         self.bridge = CvBridge()
-        self.template = cv2.imread(template_image_path, cv2.IMREAD_GRAYSCALE)
+        #use imread so that is loads in grayscale already. --> might need to change if corner detection?
+        self.template = cv2.imread(TEMPLATE_IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
+
         if self.template is None:
-            self.get_logger().error(f"Failed to load template image: {template_image_path}")
+            self.get_logger().error(f"Failed to load template image: {TEMPLATE_IMAGE_PATH}")
             exit(1)
 
-        self.get_logger().infor("KeyboardAlignmentNode running")
+        self.get_logger().info("KeyboardAlignmentNode running")
 
     def image_callback(self, msg):
-
-        sift = self.sift_detector(msg.data)
+        #need to fix msg.data beforehand
+        sift_output = self.sift_detector(msg.data)
         #corner = self.corner_detection(msg.data)
 
-        self.publisher_.publish(sift)
-
-        self.get_logger().info(f'Sift output: "{sift}"')
+        #self.publisher_.publish(sift_output)
+        self.get_logger().info(f'Sift output: "{sift_output}"')
 
     def corner_detection(self):
         return
     
-    def sift_detector(self):
+    def sift_detector(self, gray_frame):
+        #initialize sift detector
+        sift = cv2.SIFT_create()
+
+        #TEMPLATE:
+        #find keypoints of template image (grayed out version) --> can put a mask
+        template_keypoints = sift.detect(self.template, None)
+        self.get_logger.info(f"Number of Template Keypoints: {len(template_keypoints)}")
+        #draw those keypoints --> use flags for better keypoints
+        template_image_keypoints = cv2.drawKeypoints(self.template, template_keypoints, None)
+        #make a new image with the keypoints on it
+        cv2.imwrite('sift_keypoints_template_image.jpg', template_image_keypoints)
+
+        #FRAME:
+        #do the same
+        frame_keypoints = sift.detect(gray_frame, None)
+        self.get_logger.info(f"Number of Frame Keypoints: {len(frame_keypoints)}")
+        frame_image_keypoints = cv2.drawKeypoints(gray_frame, frame_keypoints, None)
+        cv2.imwrite('sift_frame_image_keypoints.jpg', frame_image_keypoints)
         return
