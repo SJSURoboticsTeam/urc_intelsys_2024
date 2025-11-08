996081b4dc6708ec91d6735ef25d3ae8cdf72f68
japjikbatra <japjikbatra@gmail.com>
2025-11-08 19:28:35 +0000

    wrote aruco tag logic

--- src/keyboard_typing/keyboard_typing/ArucoAlignment.py
+++ src/keyboard_typing/keyboard_typing/ArucoAlignment.py
@@ -22,9 +22,14 @@ class ArucoAlignment(Node):
         self.subscriber = self.create_subscription(Image, IMAGE_TOPIC, self.image_callback, QOS)
         self.publisher = self.create_publisher(PoseStamped, 'autonomous_typing', QOS)
 
-        self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+        #self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
         self.parameters = aruco.DetectorParameters()
 
+        #camera stuff needed to determine position:
+        self.camera_matrix = np.load('camera_matrix.npy')
+        self.dist_coeffs = np.load('dist_coeffs.npy')
+
+    #figure out which aruco dictionary to use --> do we know this beforehand?
     def detect_aruco_dictionary(gray_image):
         for d in POSSIBLE_DICTS:
             aruco_dict = aruco.getPredefinedDictionary(d)
@@ -39,6 +44,7 @@ class ArucoAlignment(Node):
         return None, None, None
                 
 
+    #get image, detect aruco tags, estimate pose, publish pose
     def image_callback (self, msg):
         #convert ROS2 Image to OpenCV image --> convert to grayscale
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -46,8 +52,57 @@ class ArucoAlignment(Node):
 
         corners, ids, _ = self.detect_aruco_dictionary(gray_image)
 
-        if ids is None:
+        if ids is None or len(ids) < 4: #do we need exactly 4, or can we do with 3?
+            self.get_logger().info("No aruco tags detected.")
             return
+        
+        #estimating tag poses
+        rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 0.02, self.camera_matrix, self.distCoeffs)
+
+        tag_positions = {int(ids[i]): tvecs[i][0] for i in range(len(ids))}
+
+        required_tags = [0, 1, 2, 3]
+
+        if all(t in tag_positions for t in required_tags):
+            pts = np.array([tag_positions[t] for t in required_tags])
+            self.compute_pose(pts)
+    
+    #computes pose
+    def compute_pose(self, pts):
+        #compute normal vector for plane orientation
+        v1 = pts[1] - pts[0]
+        v2 = pts[3] - pts[0]
+        normal = np.cross(v1, v2)
+        normal /= np.linalg.norm(normal)
+
+        #compute center of keyboard which is just the average of the 4 tags
+        center = np.mean(pts, axis=0)
+
+        #target position: how far we want to be away from keyboard
+        offset_distance = 0.30
+        target_position = center - normal * offset_distance
+
+        #calculate yaw from normal
+        yaw = atan2(normal[0], normal[2])
+
+        #convert to quaternion?
+
+        #publish pose message
+        pose = PoseStamped()
+
+        self.publisher_.publish(pose)
+        self.get_logger().info(f"Published target pose: pose = {target_position}")
+
         
         #cv2.Mat.outputImage = gray_image.clone();
         #cv2.aruco.drawDetectedMarkers(outputImage, markerCorners, markerIds);
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ArucoAlignment()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == 'main':
+    main()
